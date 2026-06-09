d65e848efc3b953a5cc48f6c3b0513ab535075c7
ElRunti <ruben060805@outlook.com>
2026-06-08 20:45:03 -0600

    feat: "agrega Proyecto de Manejo de Estado"

---
+++ Proyecto_Manejo_Estado/lista-compras/src/ListaCompras.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+function ListaCompras() {
+  // Definir el estado para la lista de compras
+  const [productos, setProductos] = useState([]);
+  const [productosComprados, setProductosComprados] = useState([]);
+  const [nuevoProducto, setNuevoProducto] = useState("");
+
+  // Función para agregar un nuevo producto a la lista
+  const agregarProducto = () => {
+    if (nuevoProducto.trim() !== "") {
+      setProductos([...productos, nuevoProducto]);
+      setNuevoProducto("");
+    }
+  };
+
+  // Función para eliminar un producto de la lista
+  const eliminarProducto = (index) => {
+        setProductos(productos.filter((_, i) => i !== index))
+    // Completar la lógica para eliminar un producto
+  };
+  //funcion para pasar el producto a comprado
+  const actualizarProducto = (index) =>{
+    setProductosComprados([...productosComprados, productos[index]])
+    setProductos(productos.filter((_, i) => i !== index))
+    
+  }
+
+  return (
+    <div>
+      <h2>Lista de Compras</h2>
+      <input
+        type="text"
+        value={nuevoProducto}
+        onChange={(e) => setNuevoProducto(e.target.value)}
+      />
+      <button onClick={agregarProducto}>Agregar</button>
+      <ul>
+        {productos.map((producto, index) => (
+          <li key={index}>
+            {producto}
+            <button onClick={() => eliminarProducto(index)}>Eliminar</button>
+            <button onClick={() => actualizarProducto(index)}>Comprado</button>
+          </li>
+        ))}
+      </ul>
+
+      <h3>Productos Comprados</h3>
+      <ul>
+        {productosComprados.map((producto, index) => (
+        <li key={index}>
+        {producto}
+        </li>
+    ))}
+    </ul>
+    </div>
+  );
+}
+
+export default ListaCompras;
